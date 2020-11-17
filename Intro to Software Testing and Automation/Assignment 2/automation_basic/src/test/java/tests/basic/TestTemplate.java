64e2d8b09b0496408510b0057a2423b2ff0602be
Gavi Schneider <gavischneider@gmail.com>
2020-11-17 22:53:33 +0200

    Finished automation - assignment 2

--- Intro to Software Testing and Automation/Assignment 2/automation_basic/src/test/java/tests/basic/TestTemplate.java
+++ Intro to Software Testing and Automation/Assignment 2/automation_basic/src/test/java/tests/basic/TestTemplate.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.JavascriptExecutor;
@@ -18,7 +20,8 @@ import java.util.*;
 
 public class TestTemplate extends TestBase {
 	private WebDriver driver;
-	  private Map<String, Object> vars;
+	  @SuppressWarnings("unused")
+	private Map<String, Object> vars;
 	  JavascriptExecutor js;
 	  @Before
 	  public void setUp() {
@@ -35,8 +38,6 @@ public class TestTemplate extends TestBase {
 	@Test
 	public void test() {
 		try {
-
-			//app.getDriver().get("http://selenium2.ru/");
 			app.driver.get("https://demo.nopcommerce.com/");
 		    app.driver.manage().window().setSize(new Dimension(1920, 1080));
 		    app.driver.findElement(By.linkText("Log in")).click();
@@ -50,7 +51,25 @@ public class TestTemplate extends TestBase {
 		    app.driver.findElement(By.cssSelector(".item-box:nth-child(1) img")).click();
 		    app.driver.findElement(By.cssSelector("#product_attribute_input_3 li:nth-child(1) > label")).click();
 		    app.driver.findElement(By.id("add-to-cart-button-1")).click();
+		    
+		    Thread.sleep(3000);
+		    String itemPrice = app.driver.findElement(By.cssSelector("#price-value-1")).getText();
+		    Thread.sleep(3000);
+		    String cartQuantity = app.driver.findElement(By.cssSelector("#topcartlink > a > span.cart-qty")).getText();
+		    
+		    if (!cartQuantity.equals("(1)")) {
+		    	throw new Exception("Cart quantity is not equal to 1!");
+		    }
+		    
+		    Thread.sleep(3000);
 		    app.driver.findElement(By.cssSelector(".cart-label")).click();
+		    Thread.sleep(3000);
+		    String cartTotal = app.driver.findElement(By.cssSelector("#shopping-cart-form > div.cart-footer > div.totals > div.total-info > table > tbody > tr.order-total > td.cart-total-right > span > strong")).getText();
+		    
+		    if (!itemPrice.equals(cartTotal)) {
+		    	throw new Exception("Item Price and Cart total do not match!");
+		    };
+		    
 		    app.driver.findElement(By.id("termsofservice")).click();
 		    app.driver.findElement(By.id("checkout")).click();
 		    app.driver.findElement(By.cssSelector(".new-address-next-step-button:nth-child(1)")).click();
@@ -61,15 +80,37 @@ public class TestTemplate extends TestBase {
 		    Thread.sleep(3000);
 		    app.driver.findElement(By.cssSelector(".payment-info-next-step-button")).click();
 		    Thread.sleep(3000);
+		    String finalTotal = app.driver.findElement(By.cssSelector("#shopping-cart-form > div.table-wrapper > table > tbody > tr > td.unit-price > span")).getText();
+		    if (!finalTotal.equals(cartTotal)) {
+		    	throw new Exception("Final Total and Cart Total do not match!");
+		    }
 		    app.driver.findElement(By.cssSelector(".confirm-order-next-step-button")).click();
 		    Thread.sleep(3000);
 		    {
-		      WebElement element = app.driver.findElement(By.cssSelector(".order-completed-continue-button"));
-		      Actions builder = new Actions(driver);
+		    	WebElement element = app.driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div > div > div > div.page-body.checkout-data > div > div.buttons > input"));
+		      Actions builder = new Actions(app.driver);
 		      builder.moveToElement(element).perform();
 		    }
 		    Thread.sleep(3000);
+		    String message = app.driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div > div > div > div.page-title > h1")).getText();
+		    
+		    if (!message.equals("Thank you")) {
+		    	throw new Exception("Message does not say thank you!");
+		    }
+		    
+		    Thread.sleep(3000);
 		    app.driver.findElement(By.cssSelector(".order-completed-continue-button")).click();
+		    String newMessage = app.driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div > div > div > div > div.topic-block > div.topic-block-title > h2")).getText();
+		    
+		    if (!newMessage.equals("Welcome to our store")) {
+		    	throw new Exception("New message does not say welcome to our store!");
+		    }
+		    
+		    String newCartQuantity = app.driver.findElement(By.cssSelector("#topcartlink > a > span.cart-qty")).getText();
+		    
+		    if (!newCartQuantity.equals("(0)")) {
+		    	throw new Exception("New cart quantity does not equal 0!");
+		    }
 		    app.driver.close();
 		  
 
@@ -79,6 +120,6 @@ public class TestTemplate extends TestBase {
 			onTestFailure(t);
 		}
 	}
-
+	
 
 }
