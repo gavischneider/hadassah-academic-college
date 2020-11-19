b8b959fd3d9b3948ad2e06920c0dc6d95f1d3d93
Gavi Schneider <gavischneider@gmail.com>
2020-11-19 19:47:03 +0200

    Errors are all taken care of

--- Web Development A/Assignment 2/script.js
+++ Web Development A/Assignment 2/script.js
@@ -7,13 +7,29 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function handleSubmit() {
+  // Delete existing error messages (if they exist)
+  let listOfErrors = document.getElementById("list-of-errors");
+  if (listOfErrors) {
+    listOfErrors.parentElement.removeChild(listOfErrors);
+  }
+
   // Get all the inputs
   let errors = [];
-  let firstName = document.getElementById("first-name-input").value;
-  let lastName = document.getElementById("last-name-input").value;
-  let birthYear = document.getElementById("birth-year-input").value;
-  let immigrationYear = document.getElementById("immigration-year-input").value;
+  let firstName = document.getElementById("first-name-input").value.trim();
+  let lastName = document.getElementById("last-name-input").value.trim();
+  let birthYear = document.getElementById("birth-year-input").value.trim();
+  let immigrationYear = document
+    .getElementById("immigration-year-input")
+    .value.trim();
+  let childInputs = Array.from(document.querySelectorAll(".child-input"));
 
+  childInputs = childInputs.map((c) => {
+    return c.value.trim();
+  });
+
+  const currentYear = new Date().getFullYear();
+
+  // Check which errors we need to display
   if (!firstName) {
     errors.push("First name is missing");
   }
@@ -23,11 +39,57 @@ function handleSubmit() {
   if (!birthYear) {
     errors.push("Birth year is missing");
   }
+  if (isNaN(birthYear)) {
+    errors.push("Birth year should be a number");
+  }
+  if (birthYear && birthYear < 1900) {
+    errors.push("Birth year should be at least 1900");
+  }
+  if (birthYear > currentYear) {
+    errors.push("Birth year can not be after the current year");
+  }
   if (!immigrationYear) {
     errors.push("Immigration year is missing");
   }
-  if (birthYear > immigrationYear) {
-    errors.push("Immigration year should be after birth");
+  if (isNaN(immigrationYear)) {
+    errors.push("Immigration year should be a number");
+  }
+  if (immigrationYear && immigrationYear < 1900) {
+    errors.push("Immigration year should be at least 1900");
+  }
+  if (immigrationYear > currentYear) {
+    errors.push("Immigration year can not be after the current year");
+  }
+  if (immigrationYear && birthYear > immigrationYear) {
+    errors.push("Immigration year should be after birth year");
+  }
+
+  // Check if a child name is missing
+  let needChildError = false;
+  childInputs.map((child) => {
+    if (!child.value) {
+      needChildError = true;
+    }
+    console.log("CHILD VALUE");
+    console.log(child);
+  });
+  if (needChildError) {
+    errors.push("Child name is missing");
+  }
+
+  // Check if child names are not unique
+  let bool = false;
+  for (i = 0; i < childInputs.length; i++) {
+    for (j = 0; j < childInputs.length; j++) {
+      if (i !== j) {
+        if (childInputs[i].value === childInputs[j].value) {
+          bool = true;
+        }
+      }
+    }
+  }
+  if (bool) {
+    errors.push("Child names should be different");
   }
 
   console.log("Variables:");
@@ -41,6 +103,7 @@ function handleSubmit() {
 
   if (errors.length > 0) {
     listOfErrors = document.createElement("ul");
+    listOfErrors.setAttribute("id", "list-of-errors");
     errors.map((error) => {
       newError = document.createElement("li");
       newError.textContent = error;
@@ -49,11 +112,13 @@ function handleSubmit() {
     let warningMessage = document.getElementById("warning-message");
     warningMessage.appendChild(listOfErrors);
     warningMessage.style.display = "block";
+  } else {
+    let warningMessage = document.getElementById("warning-message");
+    warningMessage.style.display = "none";
   }
 }
 
 function handleKeyUp() {
-  console.log("Key down");
   let numOfChildren = document.getElementById("num-of-children-input").value;
 
   console.log("numOfChildren / NUM_OF_CHILDREN_DIVS");
@@ -64,6 +129,9 @@ function handleKeyUp() {
     let childrenForm = document.getElementById("children-form");
     if (numOfChildren > NUM_OF_CHILDREN_DIVS) {
       // We need more divs for children
+      if (numOfChildren > 10) {
+        numOfChildren = 10;
+      }
       while (numOfChildren > NUM_OF_CHILDREN_DIVS) {
         let newChildDiv = document.createElement("div");
         newChildDiv.setAttribute("class", "child-div");
@@ -73,6 +141,7 @@ function handleKeyUp() {
         let input = document.createElement("input");
         input.setAttribute("type", "text");
         input.setAttribute("class", "form-control");
+        input.setAttribute("class", "child-input");
         newChildDiv.appendChild(input);
         childrenForm.appendChild(newChildDiv);
         NUM_OF_CHILDREN_DIVS++;
