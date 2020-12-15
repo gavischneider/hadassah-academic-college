91be178177b62daddd7bd0e57fac8212f4a74a26
Gavi Schneider <gavischneider@gmail.com>
2020-12-15 20:21:44 +0200

    Added readme

--- Web Development A/Assignment 3/script.js
+++ Web Development A/Assignment 3/script.js
@@ -1,193 +1,211 @@
-window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("imageContainer").style.display = "none";
-  document.getElementById("warningMessage").style.display = "none";
-  document
-    .getElementById("submitButton")
-    .addEventListener("click", handleSubmit);
-  document
-    .getElementById("showWeatherButton")
-    .addEventListener("click", fetchWeather);
-});
-
-function handleSubmit() {
-  removeErrorsFromDOM();
-
-  let name = document.getElementById("nameInput").value.trim();
-  let latitude = document.getElementById("latitudeInput").value.trim();
-  let longitude = document.getElementById("longitudeInput").value.trim();
-
-  let errors = validateForm(name, latitude, longitude);
-  if (errors.length > 0) {
-    displayErrors(errors);
-  } else {
-    // If we get here, there are no errors, add the new location to list
-    document.getElementById("message").style.display = "none";
-    addNewLocationToList(name, latitude, longitude);
-    cleanInputs();
-  }
-}
-
-function removeErrorsFromDOM() {
-  warningMessage = document.getElementById("warningMessage").style.display =
-    "none";
-  let listOfErrors = document.getElementById("list-of-errors");
-  if (listOfErrors) {
-    listOfErrors.parentElement.removeChild(listOfErrors);
-  }
-}
-
-function validateForm(name, latitude, longitude) {
-  // Validate
-  let errors = [];
-
-  if (!name) {
-    errors.push("First name is missing");
-  }
-  if (!latitude) {
-    errors.push("Latitude is missing");
-  }
-  if (!longitude) {
-    errors.push("Longitude is missing");
-  }
-  if (isNaN(latitude)) {
-    errors.push("Latitude must be a number");
-  }
-  if (isNaN(longitude)) {
-    errors.push("Longitude must be a number");
-  }
-  if (latitude && (latitude > 90 || latitude < -90)) {
-    errors.push("Latitude must be a number between +90 and -90");
-  }
-  if (longitude && (longitude > 180 || longitude < -180)) {
-    errors.push("Longitude must be a number between +180 and -180");
-  }
-  return errors;
-}
-
-function displayErrors(errors) {
-  listOfErrors = document.createElement("ul");
-  listOfErrors.setAttribute("id", "list-of-errors");
-  errors.map((error) => {
-    newError = document.createElement("li");
-    newError.textContent = error;
-    listOfErrors.appendChild(newError);
+(() => {
+  window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("imageContainer").style.display = "none";
+    document.getElementById("warningMessage").style.display = "none";
+    document
+      .getElementById("submitButton")
+      .addEventListener("click", handleSubmit);
+    document
+      .getElementById("showWeatherButton")
+      .addEventListener("click", fetchWeather);
   });
-  let warningMessage = document.getElementById("warningMessage");
-  warningMessage.appendChild(listOfErrors);
-  warningMessage.style.display = "block";
-}
 
-function addNewLocationToList(name, latitude, longitude) {
-  let locationList = document.getElementById("list-tab");
-  let location = document.createElement("div");
-  location.classList.add("locationItem");
-  location.classList.add("list-group-item");
-  location.classList.add("list-group-item-action");
-  location.classList.add("d-flex");
-  location.classList.add("align-items-center");
-  location.classList.add("justify-content-between");
-  location.setAttribute("data-toggle", "list");
-  location.setAttribute("role", "tab");
-  let button = document.createElement("button");
-  button.setAttribute("type", "button");
-  button.classList.add("btn");
-  button.classList.add("btn-danger");
-  button.addEventListener("click", removeLocation);
-  button.textContent = "Remove";
-  location.textContent = `${name}: Latitude: ${latitude}, Longitude: ${longitude}`;
-  location.setAttribute("latitude", latitude);
-  location.setAttribute("longitude", longitude);
-  location.appendChild(button);
-  locationList.appendChild(location);
-}
+  // Data structure to hold locations
+  let locations = [];
 
-function removeLocation(event) {
-  event.target.parentElement.parentElement.removeChild(
-    event.target.parentElement
-  );
-}
+  // When the user submits a new location
+  function handleSubmit() {
+    removeErrorsFromDOM();
 
-function cleanInputs() {
-  document.getElementById("nameInput").value = "";
-  document.getElementById("latitudeInput").value = "";
-  document.getElementById("longitudeInput").value = "";
-}
+    let name = document.getElementById("nameInput").value.trim();
+    let latitude = document.getElementById("latitudeInput").value.trim();
+    let longitude = document.getElementById("longitudeInput").value.trim();
 
-async function fetchWeather() {
-  // Check if a list item is 'active'
-  let locationItems = Array.from(document.querySelectorAll(".locationItem"));
-  if (locationItems.length > 0) {
-    let activeItem = locationItems.filter((location) => {
-      return location.classList.contains("active");
+    let errors = validateForm(name, latitude, longitude);
+    if (errors.length > 0) {
+      displayErrors(errors);
+    } else {
+      // If we get here, there are no errors, add the new location to list
+      document.getElementById("message").style.display = "none";
+      addNewLocationToList(name, latitude, longitude);
+      cleanInputs();
+    }
+  }
+
+  // Clean up all visible errors
+  function removeErrorsFromDOM() {
+    warningMessage = document.getElementById("warningMessage").style.display =
+      "none";
+    let listOfErrors = document.getElementById("list-of-errors");
+    if (listOfErrors) {
+      listOfErrors.parentElement.removeChild(listOfErrors);
+    }
+  }
+
+  // Verify that everything is as expected
+  function validateForm(name, latitude, longitude) {
+    // Validate
+    let errors = [];
+
+    if (!name) {
+      errors.push("First name is missing");
+    }
+    if (!latitude) {
+      errors.push("Latitude is missing");
+    }
+    if (!longitude) {
+      errors.push("Longitude is missing");
+    }
+    if (isNaN(latitude)) {
+      errors.push("Latitude must be a number");
+    }
+    if (isNaN(longitude)) {
+      errors.push("Longitude must be a number");
+    }
+    if (latitude && (latitude > 90 || latitude < -90)) {
+      errors.push("Latitude must be a number between +90 and -90");
+    }
+    if (longitude && (longitude > 180 || longitude < -180)) {
+      errors.push("Longitude must be a number between +180 and -180");
+    }
+
+    // Check if location already exists
+    locations.map((location) => {
+      if (location.latitude === latitude && location.longitude === longitude) {
+        errors.push("You have already entered these coordinates");
+      }
     });
-    if (activeItem) {
-      // fix this
-      // We found an 'active' item
-      const latitude = activeItem[0].attributes.latitude.nodeValue;
-      const longitude = activeItem[0].attributes.longitude.nodeValue;
-      try {
-        const weather = await getWeather(latitude, longitude);
-        const weatherJson = await weather.json();
-        console.log("-----WEATHER-----");
-        console.log(weatherJson);
-        console.log("-----------------");
-        console.log(JSON.stringify(weatherJson));
+    return errors;
+  }
 
-        // Build the image url and place it in the DOM
-        let imageURL = `http://www.7timer.info/bin/astro.php?lon=${longitude}&amp;lat=${latitude}&amp;ac=0&amp;lang=en&amp;unit=metric&amp;output=internal&amp;tzshift=0`;
-        document.getElementById("weatherImage").setAttribute("src", imageURL);
-        document.getElementById("imageContainer").style.display = "block";
+  // Display relevant errors
+  function displayErrors(errors) {
+    listOfErrors = document.createElement("ul");
+    listOfErrors.setAttribute("id", "list-of-errors");
+    errors.map((error) => {
+      newError = document.createElement("li");
+      newError.textContent = error;
+      listOfErrors.appendChild(newError);
+    });
+    let warningMessage = document.getElementById("warningMessage");
+    warningMessage.appendChild(listOfErrors);
+    warningMessage.style.display = "block";
+  }
 
-        // Add today's and tommorow's weather to DOM
-        let dataSeries = weatherJson.dataseries;
-        console.log("-----DATASERIES-----");
-        console.log(dataSeries);
+  // Add a new location to DOM and to data structure
+  function addNewLocationToList(name, latitude, longitude) {
+    let locationList = document.getElementById("list-tab");
+    let location = document.createElement("div");
+    location.classList.add("locationItem");
+    location.classList.add("list-group-item");
+    location.classList.add("list-group-item-action");
+    location.classList.add("d-flex");
+    location.classList.add("align-items-center");
+    location.classList.add("justify-content-between");
+    location.setAttribute("data-toggle", "list");
+    location.setAttribute("role", "tab");
+    let button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.classList.add("btn");
+    button.classList.add("btn-danger");
+    button.addEventListener("click", removeLocation);
+    button.textContent = "Remove";
+    location.textContent = `${name}: Latitude: ${latitude}, Longitude: ${longitude}`;
+    location.setAttribute("latitude", latitude);
+    location.setAttribute("longitude", longitude);
+    location.appendChild(button);
+    locationList.appendChild(location);
 
-        const todayWeather = dataSeries[0].weather;
-        const todayTempMin = dataSeries[0].temp2m.min;
-        const todayTempMax = dataSeries[0].temp2m.max;
-        const todayWind = dataSeries[0].wind10m_max;
+    let newLocation = {
+      name: name,
+      latitude: latitude,
+      longitude: longitude,
+    };
 
-        const tomorrowWeather = dataSeries[1].weather;
-        const tomorrowTempMin = dataSeries[1].temp2m.min;
-        const tomorrowTempMax = dataSeries[1].temp2m.max;
-        const tomorrowWind = dataSeries[1].wind10m_max;
+    locations.push(newLocation);
+  }
 
-        let weatherList = Array.from(document.querySelectorAll(".weatherList"));
+  // When the user removes a location
+  function removeLocation(event) {
+    event.target.parentElement.parentElement.removeChild(
+      event.target.parentElement
+    );
+  }
 
-        //weatherList[0].textContent = `Today's Weather: ${todayDate}`;
-        weatherList[1].textContent = `Weather Forecast: ${todayWeather}`;
-        weatherList[2].textContent = `Temperature: Between ${todayTempMin} and ${todayTempMax} (Cel)`;
-        weatherList[3].textContent = `Wind Factor: ${todayWind}`;
+  // Empty out the inputs
+  function cleanInputs() {
+    document.getElementById("nameInput").value = "";
+    document.getElementById("latitudeInput").value = "";
+    document.getElementById("longitudeInput").value = "";
+  }
 
-        //weatherList[4].textContent = `Tomorrow's Weather: ${tomorrowDate}`;
-        weatherList[5].textContent = `Weather Forecast: ${tomorrowWeather}`;
-        weatherList[6].textContent = `Temperature: Between ${tomorrowTempMin} and ${tomorrowTempMax} (Cel)`;
-        weatherList[7].textContent = `Wind Factor: ${tomorrowWind}`;
+  // Get the weather
+  async function fetchWeather() {
+    // Check if a list item is 'active'
+    let locationItems = Array.from(document.querySelectorAll(".locationItem"));
+    if (locationItems.length > 0) {
+      let activeItem = locationItems.filter((location) => {
+        return location.classList.contains("active");
+      });
+      if (activeItem) {
+        // fix this
+        // We found an 'active' item
+        const latitude = activeItem[0].attributes.latitude.nodeValue;
+        const longitude = activeItem[0].attributes.longitude.nodeValue;
+        try {
+          const weather = await getWeather(latitude, longitude);
+          const weatherJson = await weather.json();
 
-        console.log(arr);
-      } catch (error) {
-        console.log(console.error());
+          // Build the image url and place it in the DOM
+          let imageURL = `http://www.7timer.info/bin/astro.php?lon=${longitude}&amp;lat=${latitude}&amp;ac=0&amp;lang=en&amp;unit=metric&amp;output=internal&amp;tzshift=0`;
+          document.getElementById("weatherImage").setAttribute("src", imageURL);
+          document.getElementById("imageContainer").style.display = "block";
+
+          // Add today's and tommorow's weather to DOM
+          let dataSeries = weatherJson.dataseries;
+
+          const todayWeather = dataSeries[0].weather;
+          const todayTempMin = dataSeries[0].temp2m.min;
+          const todayTempMax = dataSeries[0].temp2m.max;
+          const todayWind = dataSeries[0].wind10m_max;
+
+          const tomorrowWeather = dataSeries[1].weather;
+          const tomorrowTempMin = dataSeries[1].temp2m.min;
+          const tomorrowTempMax = dataSeries[1].temp2m.max;
+          const tomorrowWind = dataSeries[1].wind10m_max;
+
+          let weatherList = Array.from(
+            document.querySelectorAll(".weatherList")
+          );
+
+          weatherList[1].textContent = `Weather Forecast: ${todayWeather}`;
+          weatherList[2].textContent = `Temperature: Between ${todayTempMin} and ${todayTempMax} (Cel)`;
+          weatherList[3].textContent = `Wind Factor: ${todayWind}`;
+
+          weatherList[5].textContent = `Weather Forecast: ${tomorrowWeather}`;
+          weatherList[6].textContent = `Temperature: Between ${tomorrowTempMin} and ${tomorrowTempMax} (Cel)`;
+          weatherList[7].textContent = `Wind Factor: ${tomorrowWind}`;
+        } catch (error) {
+          console.log(`Error: ${error}`);
+        }
       }
     }
   }
-}
 
-async function getWeather(latitude, longitude) {
-  let response = await fetch(
-    "http://www.7timer.info/bin/api.pl?" +
-      new URLSearchParams({
-        lon: longitude,
-        lat: latitude,
-        product: "civillight",
-        output: "json",
-      })
-  );
-  return response;
-}
-// http://www.7timer.info/bin/api.pl?lon=35.213618&amp;lat=31.771959&amp;product=civillight&amp;output=json
-// lon= 35.213618
-// lat= 31.771959
-
-// pic url
-// http://www.7timer.info/bin/astro.php?lon=35.213618&amp;lat=31.771959&amp;ac=0&amp;lang=en&amp;unit=metric&amp;output=internal&amp;tzshift=0
+  // Fetch from API
+  async function getWeather(latitude, longitude) {
+    let response = await fetch(
+      "http://www.7timer.info/bin/api.pl?" +
+        new URLSearchParams({
+          lon: longitude,
+          lat: latitude,
+          product: "civillight",
+          output: "json",
+        })
+    );
+    if (!response) {
+      window.alert("There was an issue fetching the weather data");
+    }
+    return response;
+  }
+})();
