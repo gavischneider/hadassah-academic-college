f4359db0367debefa19cfb781c7d4a2637cae46b
Gavi Schneider <gavischneider@gmail.com>
2020-12-26 16:12:27 +0200

    Added register view

---
+++ Web Development A/Assignment 4/routes/auth.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("Auth routes");
+});
+
+// Home route
+router.get("/register", (req, res) => {
+  res.render("register", { title: "register" });
+});
+
+module.exports = router;
