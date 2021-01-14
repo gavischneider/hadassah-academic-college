ad7e66f97431e0095a1ad571d8d66cfc45bc504c
Gavi Schneider <gavischneider@gmail.com>
2021-01-14 17:31:03 +0200

    Added auth controllers

---
+++ Web Development A/Assignment 4/controllers/authControllers.js
@@ -0,0 +1,90 @@
+const User = require("../models").User;
+const Location = require("../models").Location;
+
+const authController = {
+  password(req, res) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const password = req.body.password;
+    User.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    })
+      .then((res) => {
+        res.send(res);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  },
+
+  login(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({
+      where: { email, password },
+    })
+      .then(async (user) => {
+        console.log("*USER*");
+        console.log(user);
+
+        if (user) {
+          console.log("Success");
+          req.session.user = user;
+
+          // We got the user, now we need to get their locations
+          const locations = await Location.findAll({
+            where: { userId: user.dataValues.id },
+          });
+
+          req.session.user.locations = Array.from(locations);
+
+          const id = req.session.user.dataValues.id;
+          const firstName = req.session.user.dataValues.firstName;
+          const lastName = req.session.user.dataValues.lastName;
+          const email = req.session.user.dataValues.email;
+
+          const newUser = {
+            id,
+            firstName,
+            lastName,
+            email,
+            locations: Array.from(locations),
+          };
+
+          req.session.user = newUser;
+
+          console.log("Login success, redirecting to index");
+          return res.redirect("/");
+        } else {
+          console.log("Failure");
+
+          // We could not find a user with that email/password
+          res.render("login", { title: "login", message: "Error logging in" });
+        }
+      })
+      .catch((err) => {
+        console.log(`Error while searching for user: ${err}`);
+      });
+  },
+
+  logout(req, res) {
+    console.log("LOGOUT CONTROLLER");
+    //req.logOut();
+    req.session.destroy(function (err) {
+      req.session = null;
+      if (err) {
+        console.log(err);
+      } else {
+        //res.redirect(200, "login", { title: "login", message: "login-message" });
+        return res.redirect("/");
+        //res.render("login", { title: "login", message: "Some Message" });
+      }
+    });
+  },
+};
+
+module.exports = authController;
--- Web Development A/Assignment 4/routes/auth.js
+++ Web Development A/Assignment 4/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models").User;
 const Location = require("../models").Location;
+const authController = require("../controllers/authControllers");
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
@@ -23,154 +24,102 @@ router.get("/login", (req, res) => {
 });
 
 // Create new user
-router.post("/password", (req, res) => {
-  // console.log("-----req: =====");
-  // console.log(req);
-  // console.log("-----req.body: =====");
-  // console.log(req.body);
-  // console.log("-----req.query: =====");
-  // console.log(req.query);
-  // console.log("-----req.params: =====");
-  // console.log(req.params);
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-  const password = req.body.password;
-  User.create({
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password,
-  })
-    .then((res) => {
-      // Store the user in the session
-      //req.session.user = req.body;
-      // Remove password so its not in the session
-      //req.session.user.dataValues.password = null;
+router.post("/password", authController.password);
+// router.post("/password", (req, res) => {
 
-      // const id = req.session.user.dataValues.id;
-      // const firstName = req.session.user.dataValues.firstName;
-      // const lastName = req.session.user.dataValues.lastName;
-      // const email = req.session.user.dataValues.email;
+//   const firstName = req.body.firstName;
+//   const lastName = req.body.lastName;
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   User.create({
+//     firstName: firstName,
+//     lastName: lastName,
+//     email: email,
+//     password: password,
+//   })
+//     .then((res) => {
 
-      // const newUser = {
-      //   id,
-      //   firstName,
-      //   lastName,
-      //   email,
-      //   locations: Array.from(locations),
-      // };
-
-      // req.session.user = newUser;
-
-      res.send(res);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
+//       res.send(res);
+//     })
+//     .catch((err) => {
+//       res.send(err);
+//     });
+// });
 
 // Login route
-router.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  User.findOne({
-    where: { email, password },
-  })
-    .then(async (user) => {
-      console.log("*USER*");
-      console.log(user);
+router.post("/login", authController.login);
+// router.post("/login", (req, res) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   User.findOne({
+//     where: { email, password },
+//   })
+//     .then(async (user) => {
+//       console.log("*USER*");
+//       console.log(user);
 
-      if (user) {
-        console.log("Success");
-        //console.log(user);
-        // Email and password match
-        req.session.user = user;
-        //console.log(user.dataValues.firstName);
+//       if (user) {
+//         console.log("Success");
+//         req.session.user = user;
 
-        // We got the user, now we need to get their locations
-        const locations = await Location.findAll({
-          where: { userId: user.dataValues.id },
-        });
+//         // We got the user, now we need to get their locations
+//         const locations = await Location.findAll({
+//           where: { userId: user.dataValues.id },
+//         });
 
-        //locations = await locations.json();
+//         req.session.user.locations = Array.from(locations);
 
-        //console.log("LOCATIONS!");
-        //console.log(locations);
-        req.session.user.locations = Array.from(locations);
+//         const id = req.session.user.dataValues.id;
+//         const firstName = req.session.user.dataValues.firstName;
+//         const lastName = req.session.user.dataValues.lastName;
+//         const email = req.session.user.dataValues.email;
 
-        const id = req.session.user.dataValues.id;
-        const firstName = req.session.user.dataValues.firstName;
-        const lastName = req.session.user.dataValues.lastName;
-        const email = req.session.user.dataValues.email;
+//         const newUser = {
+//           id,
+//           firstName,
+//           lastName,
+//           email,
+//           locations: Array.from(locations),
+//         };
 
-        const newUser = {
-          id,
-          firstName,
-          lastName,
-          email,
-          locations: Array.from(locations),
-        };
+//         req.session.user = newUser;
 
-        req.session.user = newUser;
+//         console.log("Login success, redirecting to index");
+//         return res.redirect("/");
+//       } else {
+//         console.log("Failure");
 
-        //console.log("req session user location");
-        //console.log(req.session.user.locations);
-
-        //console.log("REQ SESSION USERLOCATIONS LENGTH-------");
-        //console.log(req.session.user.locations.length);
-
-        //console.log("REQ SESSION USER NAME");
-        //console.log(req.session.user.dataValues);
-
-        //console.log("LOCATIONS DATAVALUES");
-        //console.log(locations.dataValues);
-
-        console.log("Login success, redirecting to index");
-        return res.redirect("/"); //, {
-        //   title: "Home",
-        //   userName: user.firstName,
-        //   locations: locations,
-        // });
-      } else {
-        console.log("Failure");
-        //console.log(user);
-        // We could not find a user with that email/password
-        //res.send(null);
-        res.render("login", { title: "login", message: "Error logging in" });
-      }
-    })
-    .catch((err) => {
-      console.log(`Error while searching for user: ${err}`);
-    });
-
-  // res.render("login", {
-  //   title: "login",
-  //   message: "You have successfully registered. Please login:",
-  // });
-});
+//         // We could not find a user with that email/password
+//         res.render("login", { title: "login", message: "Error logging in" });
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(`Error while searching for user: ${err}`);
+//     });
+// });
 
 // Logout route
-router.get("/logout", (req, res) => {
-  console.log("LOGOUT CONTROLLER");
-  //req.logOut();
-  req.session.destroy(function (err) {
-    req.session = null;
-    if (err) {
-      console.log(err);
-    } else {
-      //res.redirect(200, "login", { title: "login", message: "login-message" });
-      return res.redirect("/");
-      //res.render("login", { title: "login", message: "Some Message" });
-    }
-  });
-});
+router.get("/logout", authController.logout);
+// router.get("/logout", (req, res) => {
+//   console.log("LOGOUT CONTROLLER");
+//   //req.logOut();
+//   req.session.destroy(function (err) {
+//     req.session = null;
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       //res.redirect(200, "login", { title: "login", message: "login-message" });
+//       return res.redirect("/");
+//       //res.render("login", { title: "login", message: "Some Message" });
+//     }
+//   });
+// });
 
 // Get all users
-router.get("/getusers", (req, res) => {
-  User.findAll().then((userData) => {
-    res.json(userData);
-  });
-});
+// router.get("/getusers", (req, res) => {
+//   User.findAll().then((userData) => {
+//     res.json(userData);
+//   });
+// });
 
 module.exports = router;
