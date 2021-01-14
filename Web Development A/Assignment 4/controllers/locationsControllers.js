95b7060bd08f3e80edeaee574308eaa156f4837f
Gavi Schneider <gavischneider@gmail.com>
2021-01-14 17:37:21 +0200

    Added location controllers

---
+++ Web Development A/Assignment 4/controllers/locationsControllers.js
@@ -0,0 +1,89 @@
+const User = require("../models").User;
+const Location = require("../models").Location;
+
+const locationsController = {
+  add(req, res) {
+    const name = req.body.name;
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+    const userId = req.session.user.id;
+
+    Location.create({
+      name: name,
+      lat: latitude,
+      lon: longitude,
+      userId: userId,
+    })
+      .then((res) => {
+        // Store the user in the session
+        let newUser = {
+          ...req.session.user,
+          locations: [
+            ...req.session.user.locations,
+            { name, lat: latitude, lon: longitude },
+          ],
+        };
+
+        req.session.user = newUser;
+        res.send(res);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  },
+
+  remove(req, res) {
+    console.log(" -------> /remove route");
+    const name = req.body.name;
+    const userId = req.session.user.id;
+
+    Location.destroy({
+      where: { name, userId },
+    })
+      .then((response) => {
+        console.log(`Location removed from DB, ${response}`);
+
+        // Remove the location from session
+        let newLocations = req.session.user.locations.filter((loc) => {
+          return loc.name.localeCompare(name) !== 0;
+        });
+
+        console.log("++++++++++++++++++ NEW LOCATIONS +++++++++++++++++++");
+        console.log(newLocations);
+
+        let newUser = {
+          ...req.session.user,
+          locations: [...newLocations],
+        };
+        req.session.user = newUser;
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.log(`Error removing location from DB, ${err}`);
+        res.send(err);
+      });
+  },
+
+  removeAll(req, res) {
+    const userId = req.session.user.id;
+    Location.destroy({
+      where: { userId },
+    })
+      .then((response) => {
+        console.log(`All locations removed, ${response}`);
+
+        let newUser = {
+          ...req.session.user,
+          locations: [],
+        };
+        req.session.user = newUser;
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.log(`Error removing all locations from DB, ${err}`);
+        res.send(err);
+      });
+  },
+};
+
+module.exports = locationsController;
--- Web Development A/Assignment 4/routes/locations.js
+++ Web Development A/Assignment 4/routes/locations.js
@@ -2,92 +2,96 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models").User;
 const Location = require("../models").Location;
+const locationsController = require("../controllers/locationsControllers");
 
 router.get("/", (req, res, next) => {
   res.send("Location routes");
 });
 
-router.post("/add", (req, res, next) => {
-  const name = req.body.name;
-  const latitude = req.body.latitude;
-  const longitude = req.body.longitude;
-  const userId = req.session.user.id;
+router.post("/add", locationsController.add);
+// router.post("/add", (req, res, next) => {
+//   const name = req.body.name;
+//   const latitude = req.body.latitude;
+//   const longitude = req.body.longitude;
+//   const userId = req.session.user.id;
 
-  Location.create({
-    name: name,
-    lat: latitude,
-    lon: longitude,
-    userId: userId,
-  })
-    .then((res) => {
-      // Store the user in the session
-      let newUser = {
-        ...req.session.user,
-        locations: [
-          ...req.session.user.locations,
-          { name, lat: latitude, lon: longitude },
-        ],
-      };
+//   Location.create({
+//     name: name,
+//     lat: latitude,
+//     lon: longitude,
+//     userId: userId,
+//   })
+//     .then((res) => {
+//       // Store the user in the session
+//       let newUser = {
+//         ...req.session.user,
+//         locations: [
+//           ...req.session.user.locations,
+//           { name, lat: latitude, lon: longitude },
+//         ],
+//       };
 
-      req.session.user = newUser;
-      res.send(res);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
+//       req.session.user = newUser;
+//       res.send(res);
+//     })
+//     .catch((err) => {
+//       res.send(err);
+//     });
+// });
 
-router.post("/remove", (req, res, next) => {
-  console.log(" -------> /remove route");
-  const name = req.body.name;
-  const userId = req.session.user.id;
+router.post("/remove", locationsController.remove);
+// router.post("/remove", (req, res, next) => {
+//   console.log(" -------> /remove route");
+//   const name = req.body.name;
+//   const userId = req.session.user.id;
 
-  Location.destroy({
-    where: { name, userId },
-  })
-    .then((response) => {
-      console.log(`Location removed from DB, ${response}`);
+//   Location.destroy({
+//     where: { name, userId },
+//   })
+//     .then((response) => {
+//       console.log(`Location removed from DB, ${response}`);
 
-      // Remove the location from session
-      let newLocations = req.session.user.locations.filter((loc) => {
-        return loc.name.localeCompare(name) !== 0;
-      });
+//       // Remove the location from session
+//       let newLocations = req.session.user.locations.filter((loc) => {
+//         return loc.name.localeCompare(name) !== 0;
+//       });
 
-      console.log("++++++++++++++++++ NEW LOCATIONS +++++++++++++++++++");
-      console.log(newLocations);
+//       console.log("++++++++++++++++++ NEW LOCATIONS +++++++++++++++++++");
+//       console.log(newLocations);
 
-      let newUser = {
-        ...req.session.user,
-        locations: [...newLocations],
-      };
-      req.session.user = newUser;
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.log(`Error removing location from DB, ${err}`);
-      res.send(err);
-    });
-});
+//       let newUser = {
+//         ...req.session.user,
+//         locations: [...newLocations],
+//       };
+//       req.session.user = newUser;
+//       res.sendStatus(200);
+//     })
+//     .catch((err) => {
+//       console.log(`Error removing location from DB, ${err}`);
+//       res.send(err);
+//     });
+// });
 
-router.post("/removeall", (req, res, next) => {
-  const userId = req.session.user.id;
-  Location.destroy({
-    where: { userId },
-  })
-    .then((response) => {
-      console.log(`All locations removed, ${response}`);
+router.post("/removeall", locationsController.removeAll);
+// router.post("/removeall", (req, res, next) => {
+//   const userId = req.session.user.id;
+//   Location.destroy({
+//     where: { userId },
+//   })
+//     .then((response) => {
+//       console.log(`All locations removed, ${response}`);
 
-      let newUser = {
-        ...req.session.user,
-        locations: [],
-      };
-      req.session.user = newUser;
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.log(`Error removing all locations from DB, ${err}`);
-      res.send(err);
-    });
-});
+//       let newUser = {
+//         ...req.session.user,
+//         locations: [],
+//       };
+//       req.session.user = newUser;
+//       res.sendStatus(200);
+//     })
+//     .catch((err) => {
+//       console.log(`Error removing all locations from DB, ${err}`);
+//       res.send(err);
+//     });
+// });
 
 module.exports = router;
