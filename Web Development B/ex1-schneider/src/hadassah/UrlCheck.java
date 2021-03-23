31836727470420402d89fa942f136e95eeaa646b
Gavi Schneider <gavischneider@gmail.com>
2021-03-23 15:12:16 +0200

    Refactored - added class

--- Web Development B/ex1-schneider/src/hadassah/UrlCheck.java
+++ Web Development B/ex1-schneider/src/hadassah/UrlCheck.java
@@ -1,11 +1,5 @@
 package hadassah;
 
-import java.awt.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.UnknownHostException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class UrlCheck {
@@ -26,48 +20,17 @@ public class UrlCheck {
         String url = separatedInput[1];
         String argument = separatedInput[2];
 
-        boolean result = validateInput(command, url, argument);
-        if (!result) {
+        ValidateInput validateInput = new ValidateInput(command, url, argument);
+        boolean result = validateInput.checkCommand();
+        boolean result2 = validateInput.checkUrl();
+        boolean result3 = validateInput.checkArgument();
+
+        if(!result || !result2 || !result3) {
             System.exit(0);
         }
 
         System.out.println("Command is: " + command);
         System.out.println("URL is: " + url);
         System.out.println("Argument is: " + argument);
-    }
-
-    public boolean validateInput(String command, String url, String argument) {
-
-        // Check command
-        String[] arr = {"t", "w", "i", "l", "q"};
-        if (!Arrays.asList(arr).contains(command)){
-            System.out.println("Invalid Command!"); // todo - create exception
-            return false;
-        }
-
-        // Check URL
-        try {
-            URL urlObject = new URL(url);
-            URLConnection urlConnection = urlObject.openConnection();
-            urlConnection.connect(); // Throws UnknownHostException when it cant connect
-            System.out.println("URL exists");
-        }
-        catch(UnknownHostException e)
-        {
-            System.out.println("URL either doesn't exist or unable to connect at this moment");
-            return false;
-        }
-        catch(MalformedURLException e)
-        {
-            System.out.println("Bad URL");
-            return false;
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        // Check argument
-        return  true;
     }
 }
---
+++ Web Development B/ex1-schneider/src/hadassah/ValidateInput.java
@@ -0,0 +1,61 @@
+package hadassah;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+public class ValidateInput {
+
+    // --------- Variables ------------
+    private String command;
+    private String url;
+    private String argument;
+    public String[] commands = {"t", "w", "i", "l", "q"};
+
+    // --------- Constructors ---------
+    public ValidateInput(String theCommand, String theUrl, String theArgument) {
+        this.command = theCommand;
+        this.url = theUrl;
+        this.argument = theArgument;
+    }
+    public ValidateInput(){}
+
+    // --------- Methods --------------
+    public boolean checkCommand() {
+        if (!Arrays.asList(commands).contains(command)){
+            System.out.println("Invalid Command!"); // todo - create exception
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkUrl() {
+        try {
+            URL urlObject = new URL(url);
+            URLConnection urlConnection = urlObject.openConnection();
+            urlConnection.connect(); // Throws UnknownHostException when it cant connect
+            System.out.println("URL exists");
+        }
+        catch(UnknownHostException e)
+        {
+            System.out.println("URL either doesn't exist or unable to connect at this moment");
+            return false;
+        }
+        catch(MalformedURLException e)
+        {
+            System.out.println("Bad URL");
+            return false;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkArgument() {
+        return false;
+    }
+}
