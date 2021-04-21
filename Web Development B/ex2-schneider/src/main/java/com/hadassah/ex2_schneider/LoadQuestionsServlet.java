09664ac826eea35605687f202919fd5b3595bc4a
Gavi Schneider <gavischneider@gmail.com>
2021-04-21 19:46:32 +0300

    Successfully read text from file

--- Web Development B/ex2-schneider/src/main/java/com/hadassah/ex2_schneider/LoadQuestionsServlet.java
+++ Web Development B/ex2-schneider/src/main/java/com/hadassah/ex2_schneider/LoadQuestionsServlet.java
@@ -3,17 +3,23 @@ package com.hadassah.ex2_schneider;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 @WebServlet(name = "LoadQuestionsServlet", value = "/LoadQuestionsServlet")
 public class LoadQuestionsServlet extends HttpServlet {
 
     String file;
+    ArrayList<String> questions = new ArrayList<String>();
 
     @Override
     public void init() {
-        // application wide parameter stored in web.xml
+        // Application wide parameter stored in web.xml
         file = this.getServletContext().getInitParameter("file");
 
     }
@@ -25,9 +31,29 @@ public class LoadQuestionsServlet extends HttpServlet {
 
         // Open file
         String path = request.getServletContext().getRealPath("/docs/" + file);
-        toClient.println("Path: ");
-        toClient.println(path);
-        toClient.close();
+        File file = new File(path);
+
+        try {
+            // Check if file exists and is not a folder
+            if (file.isFile()) {
+                Scanner in = new Scanner(file);
+                while (in.hasNextLine()){
+                    Collections.addAll(questions, in.nextLine());
+                }
+                toClient.println("Questions: ");
+                toClient.println(questions);
+                toClient.close();
+            }
+            else {
+                throw new FileNotFoundException();
+            }
+        }
+        catch (FileNotFoundException e) {
+            toClient.println("Error opening file...");
+            toClient.close();
+        }
+
+
     }
 
     @Override
