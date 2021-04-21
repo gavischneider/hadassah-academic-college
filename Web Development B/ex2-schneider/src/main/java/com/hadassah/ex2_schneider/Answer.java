903f1b8a7c08e90b5fe5faeb0a597ad1d07ad267
Gavi Schneider <gavischneider@gmail.com>
2021-04-21 18:15:57 +0300

    Added question and answer classes

---
+++ Web Development B/ex2-schneider/src/main/java/com/hadassah/ex2_schneider/Answer.java
@@ -0,0 +1,16 @@
+package com.hadassah.ex2_schneider;
+
+public class Answer {
+
+    int id;
+    int question;
+    String text;
+    String user;
+
+    public Answer(int id, int question, String text, String user) {
+        this.id = id;
+        this.question = question;
+        this.text = text;
+        this.user = user;
+    }
+}
---
+++ Web Development B/ex2-schneider/src/main/java/com/hadassah/ex2_schneider/Question.java
@@ -0,0 +1,13 @@
+package com.hadassah.ex2_schneider;
+
+public class Question {
+
+    int id;
+    String text;
+    int answers = 0;
+
+    public Question(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+}
