27f2b4c94253e96282a0cad8a58c33fd22f944e8
Gavi Schneider <gavischneider@gmail.com>
2021-04-23 15:52:05 +0300

    Loop through and display questions

--- Web Development B/ex2-schneider/src/main/webapp/javascript/home.js
+++ Web Development B/ex2-schneider/src/main/webapp/javascript/home.js
@@ -1,8 +1,5 @@
-window.alert("In JS page");
-
 //(() => {
     window.addEventListener("DOMContentLoaded", () => {
-        window.alert("Working!")
         addQuestions();
     })
 
@@ -14,16 +11,50 @@ window.alert("In JS page");
             .then(res => res.json())
             .then(resp => {
 
-                let qs = "";
+                let container = document.getElementById("questionsContainer");
+                let questions = "";
                 resp.map(question => {
-                    let q = question.text;
-                    qs += q + "</br>";
+                    // div is the container for the question
+                    let div = document.createElement("div");
+
+                    // Add the question text to the container
+                    let text = document.createElement("div");
+                    text.innerHTML = question.text;
+                    text.setAttribute("class", "alert");
+                    text.setAttribute("class", "alert-primary");
+                    div.appendChild(text);
+                    let br = document.createElement("br");
+                    container.appendChild(div);
+                    container.appendChild(br);
+
+                    // Add the number of questions and two buttons - show and answer
+                    let bottomRow = document.createElement("div");
+                    bottomRow.setAttribute("class", "d-flex");
+                    let numOfAnswers = document.createElement("p");
+                    numOfAnswers.setAttribute("class", "numOfAnswers");
+                    numOfAnswers.innerText = "0 answers";
+                    let showButton = document.createElement("button");
+                    showButton.setAttribute("type", "button");
+                    showButton.setAttribute("class", "btn");
+                    showButton.setAttribute("class", "btn-primary");
+                    showButton.innerText = "Show";
+                    showButton.setAttribute("class", "showButton");
+                    let answerButton = document.createElement("button");
+                    answerButton.setAttribute("type", "button");
+                    answerButton.setAttribute("class", "btn");
+                    answerButton.setAttribute("class", "btn-info");
+                    answerButton.innerText = "Answer";
+                    answerButton.setAttribute("class", "answerButton");
+                    bottomRow.appendChild(numOfAnswers);
+                    bottomRow.appendChild(showButton);
+                    bottomRow.appendChild(answerButton);
+                    container.appendChild(bottomRow);
                 })
 
-                document.getElementById("questionsContainer").innerHTML = qs;
+                //document.getElementById("questionsContainer").innerHTML = questions;
             })
             .catch(e => {
-                document.getElementById("questionsContainer").innerHTML = "Error fetching JSON from servler...";
+                document.getElementById("questionsContainer").innerHTML = "Error fetching JSON from servlet...";
             });
     }
 //});
