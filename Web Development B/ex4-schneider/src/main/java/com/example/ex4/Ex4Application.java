efa49077d9399b023a2974dd4897444f1a9f7432
Gavi Schneider <gavischneider@gmail.com>
2021-06-02 19:44:21 +0300

    Added first template

--- Web Development B/ex4-schneider/src/main/java/com/example/ex4/Ex4Application.java
+++ Web Development B/ex4-schneider/src/main/java/com/example/ex4/Ex4Application.java
@@ -2,12 +2,20 @@ package com.example.ex4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class Ex4Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Ex4Application.class, args);
+
     }
 
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/login";
+    }
 }
